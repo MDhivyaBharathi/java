ba2f9530ec9043f4c273c3be54386d76b366742a
MDhivyaBharathi <dhivyathavasri@gmail.com>
2016-07-23 12:01:22 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,10 @@
+public class Reverse {
+
+	public static void main(String[] args) {
+		StringBuffer str=new StringBuffer("I LOVE CODING");
+		StringBuffer str2=new StringBuffer(str.reverse());
+	System.out.println("the string reverse is : "+str2);
+
+	}
+
+}
