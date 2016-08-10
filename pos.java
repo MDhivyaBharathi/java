38102913f7d5f9116bc505c38863d4e481f68b7c
MDhivyaBharathi <dhivyathavasri@gmail.com>
2016-08-10 12:02:59 +0530

    dfss
    sd

---
+++ pos.java
@@ -0,0 +1,21 @@
+package milestone2;
+public class Id {
+public static void main(String[] args) {	
+	int a;
+	a=Integer.parseInt(args[0]);{
+		if(a==0){
+			System.out.println("given is zero");
+		}
+		else if(a>0){
+			System.out.println("positive");
+		}
+		else{
+			System.out.println("negative");
+		}
+		
+	}
+	
+
+
+}
+}
