aa3f75ed5c7e89815578240587a101eea2451d6f
MDhivyaBharathi <dhivyathavasri@gmail.com>
2016-08-10 12:05:03 +0530

    odos
    od

---
+++ odd.java
@@ -0,0 +1,11 @@
+package milestone2;
+public class Id {
+public static void main(String[] args) {	
+	int a;
+	a=Integer.parseInt(args[0]);{
+		if(a%2==0){
+			System.out.println("even");
+		}
+		else{
+			System.out.println("odd");
+		}}}}
